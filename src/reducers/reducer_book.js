b22968245265a640bbb781fff60ba72432083f8c
Ricardo Hui <eehaha@gmail.com>
2018-04-23 16:30:35 +0800

    initial commit

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'Introduction to JavaScript', pages: 101},
+    {title: 'Harry Potter', pages: 30},
+    {title: 'The Dark Tower', pages: 323},
+    {title: 'Eloquent Ruby', pages: 11}
+  ];//return an array
+};
